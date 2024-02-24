e4c13be2360bd0103ad7f8991fd8d50d6a112457
Aditya Khare <adityakhare863@gmail.com>
2024-02-24 11:56:03 +0530

    bfs

--- Graphs/bfs.cpp
+++ Graphs/bfs.cpp
@@ -1,31 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void bfs(int src, const vector<vector<int>> &adj, vector<bool> &vis){
+void bfs(int src, const vector<vector<int>> &adj, vector<bool> &vis, vector<int> &bfsList)
+{
     vis[src] = true;
 
     queue<int> q;
     q.push(src);
 
-    while(!q.empty()){
+    while (!q.empty())
+    {
         int node = q.front();
         q.pop();
+        bfsList.push_back(node);
 
-        for(auto nodes: adj[node]){
-            if(!vis[nodes]){
-                bfs(nodes, adj, vis);
+        for (auto nodes : adj[node])
+        {
+            if (!vis[nodes])
+            {
+                vis[nodes] = true;
+                q.push(nodes);
             }
         }
     }
 }
 
-int main(){
+int main()
+{
     vector<vector<int>> adj;
+    // create adjacency as per need
     int n = adj.size();
-    vector<bool> vis (n, false);
-    for(int i=0; i<n; i++){
-        if(!vis[i]){
-            bfs(i, adj, vis);
+    vector<bool> vis(n, false);
+    vector<int> bfsList;
+    for (int i = 0; i < n; i++)
+    {
+        if (!vis[i])
+        {
+            bfs(i, adj, vis, bfsList);
         }
     }
     return 0;

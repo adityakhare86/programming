591a4318e85773194c5eccec32503cabd327852f
Aditya Khare <adityakhare863@gmail.com>
2024-03-16 01:47:45 +0530

    updated memoized fibonacci

--- Striver A2Z/DP/fibonnaci.cpp
+++ Striver A2Z/DP/fibonnaci.cpp
@@ -1,28 +1,31 @@
+/*
+Time (memoized): O(n)
+Space: O(n) due recursion stack space and dp array 
+*/
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution{
+class Memoized_fib{
 private:
     int find(int n, vector<int> &dp){
-        if(dp[n] != -1) return dp[n];
-        return find(n-1, dp) + find(n-2, dp);
+        if(n <= 1) return n;
+        if(dp[n] != -1) return dp[n]; 
+        return dp[n] = find(n-1, dp) + find(n-2, dp); //memoization
     }
 public:
     int fibonnaci(int n){
-        if(n == 0)return 0;
-        if(n == 1)return 1;
-
         vector<int> dp (n+1, -1);
-        dp[0] = 0; dp[1] = 1;
         return find(n, dp);
     }
 };
 
 int main(){
-    int n = 3;
+    int n = 7;
 
-    Solution obj;
-    int ans = obj.fibonnaci(n-1);
+    Memoized_fib obj;
+    int ans = obj.fibonnaci(n);
     cout<<ans<<'\n';
     return 0;
 }

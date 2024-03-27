362778d30f13a1f5b68b2aeacfeab0b40f1561d6
Aditya Khare <adityakhare863@gmail.com>
2024-03-27 14:42:43 +0530

    longest common subsequence

--- Striver A2Z/DP/DP on strings/longest_common_subsequence_lcs.cpp
+++ Striver A2Z/DP/DP on strings/longest_common_subsequence_lcs.cpp
@@ -11,7 +11,8 @@ private:
         return max(recursive_find(i1+1, i2, s1, s2), recursive_find(i1, i2+1, s1, s2));
     }
 
-    int memoized_find(int i1, int i2, const string &s1, const string &s2){
+    int memoized_find(int i1, int i2, const string &s1, 
+        const string &s2, vector<vector<int>> &dp){
         int n = s1.size(), m = s2.size();
         if(i1 >= n || i2 >= m)return 0;
         if(dp[i1][i2] != -1)return dp[i1][i2];
@@ -29,4 +30,40 @@ public:
         vector<vector<int>> dp (n, vector<int> (m, -1));
         return memoized_find(0, 0, s1, s2, dp);
     }
-}
+
+    int tabulation(string s1, string s2){
+        int n = s1.size(), m = s2.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s1[i - 1] == s2[j - 1]) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+
+    int spaceOpt(string s1, string s2){
+        int n = s1.size(), m = s2.size();
+        vector<int> dp1(m+1, 0), dp2(m+1, 0);
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s1[i - 1] == s2[j - 1]) {
+                    dp2[j] = 1 + dp1[j - 1];
+                } else {
+                    dp2[j] = max(dp1[j], dp2[j - 1]);
+                }
+            }
+            dp1 = dp2;
+            dp2.assign(m+1, 0);
+        }
+
+        return dp1[m];
+    }
+};

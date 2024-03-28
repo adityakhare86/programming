3e42c0400954b60c6f54e83ea2df88a7ec844ffc
Aditya Khare <adityakhare863@gmail.com>
2024-03-28 08:14:07 +0530

    minimum insertions to make string a palindrome

--- Striver A2Z/DP/DP on strings/minimum_insertions_to_make_string_palindrome.cpp
+++ Striver A2Z/DP/DP on strings/minimum_insertions_to_make_string_palindrome.cpp
@@ -3,5 +3,45 @@ using namespace std;
 
 class MakePalindrome{
 public:
-    
+    int tabulation(string s){
+        string s1 = s;
+        reverse(s.begin(), s.end());
+        string s2 = s;
+        int n = s1.size(), m = s2.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s1[i - 1] == s2[j - 1]) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+
+    int spaceOpt(string s){
+        string s1 = s;
+        reverse(s.begin(), s.end());
+        string s2 = s;
+        int n = s1.size(), m = s2.size();
+        vector<int> dp1(m+1, 0), dp2(m+1, 0);
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s1[i - 1] == s2[j - 1]) {
+                    dp2[j] = 1 + dp1[j - 1];
+                } else {
+                    dp2[j] = max(dp1[j], dp2[j - 1]);
+                }
+            }
+            dp1 = dp2;
+            dp2.assign(m+1, 0);
+        }
+
+        return dp1[m];
+    }
 };
